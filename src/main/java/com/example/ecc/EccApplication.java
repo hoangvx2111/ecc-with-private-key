b348ea67f74e9d7e32e51c9ceb969101a1436a48
Hoàng Võ <33387014+hoangvx2111@users.noreply.github.com>
2021-09-11 12:23:58 +0700

    Init.

---
+++ src/main/java/com/example/ecc/EccApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecc/EccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
